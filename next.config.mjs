f97a145497b9ca1659049fd0c16f8c0703a6514e
Qianxiaohou <qianxiaohou@example.com>
2026-01-09 10:31:01 +0800

    fix conflict manually

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-=======
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
->>>>>>> 9e550d53586c9edf0df63dac0ceb00258a78f207
 export default nextConfig;
